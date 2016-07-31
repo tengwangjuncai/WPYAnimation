9183bf0454381b4e4a178eebe57d14dd37cd5221
wpy <wpy@wpydemacbook-pro.local>
2016-07-31 23:42:23 +0800

    Initial Commit

---
+++ WPYAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WPYAnimation
+//
+//  Created by wpy on 16/7/31.
+//  Copyright © 2016年 wpy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WPYAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WPYAnimation
+//
+//  Created by wpy on 16/7/31.
+//  Copyright © 2016年 wpy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
